034ed4d1e8e00b7f9f0e4ec5eb6ddb7b5f5c8173
Eloi RM <eloimailmaynad@gmail.com>
2025-03-13 11:01:18 +0100

    added : route user add

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 final class UsersController extends AbstractController
 {
@@ -15,5 +16,24 @@ final class UsersController extends AbstractController
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/UsersController.php',
         ]);
+    }
+
+    #[Route('/users/add-user', name: 'add_user')]
+    public function addUser(EntityManagerInterface $entityManager): Response
+    {
+        $user = new Users();
+
+        $user->setMail('bonjour@mail.com');
+        $user->setFirstname('Toto');
+        $user->setLastname('Titi');
+        $user->setIsAdmin(false);
+        $user->setUsername('Totodu93');
+        $user->setPassword('kill *****!');
+
+        $entityManager->persist($user);
+        
+        $entityManager->flush();
+
+        return new Response('Saved new user with id '.$user->getId());
     }
 }
